7c3a1335f1aa7e4c12ef7ec485d5286e6ea088b0
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-09-09 15:58:41 +0200

    logo

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,9 +1,12 @@
-import { motion } from 'framer-motion';
-import { FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa';
-import DarkModeToggle from '../Darkmode/DarkmodeToggle'; // Assurez-vous d'ajuster ce chemin en fonction de votre structure de dossier
-import LanguageToggle from '../Translate/LanguageToggle';
+import { motion } from "framer-motion";
+import { FaGithub, FaLinkedin, FaMailBulk } from "react-icons/fa";
+import DarkModeToggle from "../Darkmode/DarkmodeToggle"; // Assurez-vous d'ajuster ce chemin en fonction de votre structure de dossier
+import LanguageToggle from "../Translate/LanguageToggle";
+import { useState } from "react"; // Ajoutez l'import pour useState
 
 const Navbar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false); // Utilisez useState pour gérer l'état du mode sombre
+
   return (
     <motion.nav
       className="p-4"
@@ -19,9 +22,7 @@ const Navbar = () => {
           animate={{ opacity: 1, x: 0 }} // Arrive à sa position normale
           transition={{ duration: 1.2 }} // Durée de l'animation
         >
-          <a href="#" style={{ fontFamily: 'chakra Petch' }} className="text-4xl md:text-6xl text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-600">
-            Yann Rezigui
-          </a>
+          <img src={isDarkMode ? "/src/assets/YR_dark_mode.png" : "/src/assets/YR_light_mode.png"} alt="" />
         </motion.div>
 
         {/* Icônes et switch */}
@@ -33,27 +34,36 @@ const Navbar = () => {
         >
           <ul className="flex flex-wrap space-x-4 md:space-x-8">
             <li>
-              <a href="#" className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-600">
+              <a
+                href="https://www.linkedin.com/in/yann-rezigui-59b79a1b0/"
+                className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-600"
+              >
                 <FaLinkedin className="text-5xl" />
               </a>
             </li>
             <li>
-              <a href="#" className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-600">
+              <a
+                href="https://github.com/YannRZG"
+                className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-600"
+              >
                 <FaGithub className="text-5xl" />
               </a>
             </li>
             <li>
-              <a href="#" className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-600">
+              <a
+                href="mailto:yann.rezigui@outlook.fr"
+                className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-600"
+              >
                 <FaMailBulk className="text-5xl" />
               </a>
             </li>
           </ul>
-          <div className='flex flex-row md:flex-row sm:flex-row items-center space-y-4 md:space-x-8 md:space-y-0'>
-          {/* Composant DarkModeToggle */}
-          <DarkModeToggle />
+          <div className="flex flex-row md:flex-row sm:flex-row items-center space-y-4 md:space-x-8 md:space-y-0">
+            {/* Composant DarkModeToggle */}
+            <DarkModeToggle />
 
-          {/* Language Toggle */}
-          <LanguageToggle />
+            {/* Language Toggle */}
+            <LanguageToggle />
           </div>
         </motion.div>
       </div>
